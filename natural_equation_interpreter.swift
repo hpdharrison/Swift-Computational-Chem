8794e2e3de9ebfe5a5f54053f845c1ae67806ee6
Henry Harrison <hpdharrison@gmail.com>
2022-06-27 21:08:15 +0100

    Added MoleculeID

--- natural_equation_interpreter.swift
+++ natural_equation_interpreter.swift
@@ -8,11 +8,9 @@ func equationInput(input: String) -> [[String]]{
     let products = reactantsProductsArray[1]
     let reactantsArray = reactants.components(separatedBy: "+")
     let productsArray = products.components(separatedBy: "+")
-    //print("Reactants Array: \(reactantsArray)")
-    //print("Products Array: \(productsArray)")
     return [reactantsArray, productsArray]
 }
-func extractMoles(targetMole: String) -> Int {
+func extractMoles(targetMole: String) -> [String] {
     let regex = try! NSRegularExpression(pattern: "(\\d*)(.+)", options: []) // Set up Regex pattern
     let range = NSRange(
         targetMole.startIndex..<targetMole.endIndex,
@@ -20,18 +18,51 @@ func extractMoles(targetMole: String) -> Int {
     ) // Create Regex object
     let matches = regex.matches(in: targetMole, options: [], range: range)
     let match = matches.first!
+    let molesInt: Int
+    let equationString: String
+    print ("target.prefix(1)")
+    if Int(target.prefix(1)) == nil {
+        molesInt = 1
+        equationString = targetMole
+    } else {
+        molesInt = Int(targetMole[Range(match.range(at: 1), in: targetMole)!])!
+        equationString = String(targetMole[Range(match.range(at: 2), in: targetMole)!])
+    }
 
-    let molesInt = Int(targetMole[Range(match.range(at: 1), in: targetMole)!])!
-    //let equationString = String(targetMole[Range(match.range(at: 2), in: targetMole)!])
-
-    return molesInt
+    return [String(molesInt), equationString]
+}
+func moleculeID(target: String) -> [[String]] {
+    let regex = try! NSRegularExpression(pattern: "([A-Z][a-z]*)(\\d*)", options: [])
+    let range = NSRange (
+        target.startIndex..<target.endIndex,
+        in: target
+    )
+    let matches = regex.matches(in: target, options: [], range: range)
+    var seperation: [[String]] = []
+    var breakdown: [String] = []
+    for match in matches {
+        breakdown = []
+        for i in 1...match.numberOfRanges-1 {
+            let attribute = target[Range(match.range(at: i), in: target)!]
+            if attribute == "" {
+                breakdown.append("1")
+            } else {
+                breakdown.append(String(attribute))
+            }
+        }
+        seperation.append(breakdown)
+    }
+    return seperation
 }
 
 print("Reaction: ", terminator: "")
 let input = readLine()
 let target = input!
 let components = equationInput(input: target)
+print(components)
 for molecule in components[0] {
     let moles = extractMoles(targetMole: molecule)
-    print("\(moles)mol of \(molecule)")
+    print("==\(moles[0])mol of \(moles[1])==")
+    print("Molecule Breakdown: ")
+    print(moleculeID(target: moles[1]))
 }
